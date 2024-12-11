3c0963f8bfe093a9e803017d0cf9e73bb71b26f6
root404-ir <mohammadrg360@gmail.com>
2024-12-12 00:42:49 +0330

    fixed sidebar links toggle

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -49,13 +49,22 @@ $(document).ready(function () {
     $('#header-profile-toggle').click(function () {
         $('#header-profile').fadeToggle()
     })
+    let isMenuOpen = false
     $('.sidebar-group-link').click(function () {
-        $('.sidebar-group-link').removeClass('sidebar-group-link-active')
         $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-down')
         $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-left')
-        $(this).addClass('sidebar-group-link-active')
-        $(this).children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-left')
-        $(this).children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-down')
+        $('.sidebar-group-link').removeClass('sidebar-group-link-active')
+        if (!isMenuOpen) {
+            isMenuOpen = true
+            $(this).addClass('sidebar-group-link-active')
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-left')
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-down')
+        } else {
+            isMenuOpen = false
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-left')
+            $(this).children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-down')
+            $(this).removeClass('sidebar-group-link-active')
+        }
     })
 })
 $('#full-screen').click(function () {
